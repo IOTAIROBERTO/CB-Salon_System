e87aff5e35bf7da139b66c1547d99fda4788de9a
IOTAI ROBERTO <124071310+IOTAIROBERTO@users.noreply.github.com>
2025-09-24 21:49:28 -0700

    .

--- salon_control_app/src/components/email/index.ts
+++
@@ -1,6 +0,0 @@
-// src/components/email/index.ts - Export file for email components
-export { default as CampaignModal } from './CampaignModal';
-export { default as EmailCampaignCard } from './EmailCampaignCard';
-export { default as EmailAutomationSettings } from './EmailAutomationSettings';
-export { default as EmailIntegration } from './EmailIntegration';
-export { default as EmailConfig } from './EmailConfig';
